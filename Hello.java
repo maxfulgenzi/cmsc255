15d56de5db38d98fbf7e9022a7499435695069a0
maxfulgenzi <75814734+maxfulgenzi@users.noreply.github.com>
2021-08-25 19:29:59 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/*************************************************
+* Hello.java
+**************************************************
+* Max Fulgenzi
+* 25 August 2021
+* CMSC 255 C90
+*************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("<Max Fulgenzi>");
+		System.out.println("<Hello.java>");
+		System.out.println("<Lab 1 Assignment>");
+		System.out.println("<25 August 2021>");
+		System.out.println("<CMSC 255 C90>");
+		System.out.println();
+	}
+}
